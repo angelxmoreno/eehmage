44100cf72c617c0f41114695db932d2b852e9235
Angel X. Moreno <angelxmoreno@gmail.com>
2020-08-18 05:42:31 -0400

    basic install of slim-php

--- public/index.php
+++ public/index.php
@@ -1,2 +1,62 @@
 <?php
-phpinfo();
+
+use Slim\Exception\HttpMethodNotAllowedException;
+use Slim\Psr7\Response;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Psr7\Request;
+use Slim\Exception\HttpNotFoundException;
+use Slim\Factory\AppFactory;
+use Slim\Middleware\ContentLengthMiddleware;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$container = new \DI\Container();
+AppFactory::setContainer($container);
+$app = AppFactory::create();
+
+$contentLengthMiddleware = new ContentLengthMiddleware();
+$app->add($contentLengthMiddleware);
+/**
+ * The routing middleware should be added before the ErrorMiddleware
+ * Otherwise exceptions thrown from it will not be handled
+ */
+$app->addRoutingMiddleware();
+
+/**
+ * Add Error Handling Middleware
+ *
+ * @param bool $displayErrorDetails -> Should be set to false in production
+ * @param bool $logErrors -> Parameter is passed to the default ErrorHandler
+ * @param bool $logErrorDetails -> Display error details in error log
+ * which can be replaced by a callable of your choice.
+ * Note: This middleware should be added last. It will not handle any exceptions/errors
+ * for middleware added after it.
+ */
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
+// Set the Not Found Handler
+$errorMiddleware->setErrorHandler(
+    HttpNotFoundException::class,
+    function (ServerRequestInterface $request, Throwable $exception, bool $displayErrorDetails) {
+        $response = new Response();
+        $response->getBody()->write('404 NOT FOUND');
+
+        return $response->withStatus(404);
+    });
+
+// Set the Not Allowed Handler
+$errorMiddleware->setErrorHandler(
+    HttpMethodNotAllowedException::class,
+    function (ServerRequestInterface $request, Throwable $exception, bool $displayErrorDetails) {
+        $response = new Response();
+        $response->getBody()->write('405 NOT ALLOWED');
+
+        return $response->withStatus(405);
+    });
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world!");
+    return $response;
+});
+
+$app->run();
