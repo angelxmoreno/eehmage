47fe3ce235a83fd2bec09458511e3d4faabf9d07
Angel X. Moreno <angelxmoreno@gmail.com>
2020-08-27 01:12:05 -0400

    added trailing slash middleware

--- config/middleware.php
+++ config/middleware.php
@@ -1,1 +1,1 @@
-<?phpdeclare(strict_types=1);namespace App;use Slim\App;use Slim\Middleware\ContentLengthMiddleware;return function (App $app) {    $app->add(new ContentLengthMiddleware);    $app->addRoutingMiddleware();};
+<?phpdeclare(strict_types=1);namespace App;use Middlewares\TrailingSlash;use Slim\App;use Slim\Middleware\ContentLengthMiddleware;return function (App $app) {    $app->add(new ContentLengthMiddleware);    $app->addRoutingMiddleware();    $app->add(new TrailingSlash(true)); // true adds the trailing slash (false removes it)};
