759cfa9c10a432c8e609acff6d59fd04c5832899
Angel X. Moreno <angelxmoreno@gmail.com>
2020-08-29 03:48:59 -0400

    Added body parsing middleware

--- config/routes.php
+++ config/routes.php
@@ -9,11 +9,11 @@ use Slim\Routing\RouteCollectorProxy;
 return function (App $app) {
     $app->get('/', Actions\HomeAction::class);
 
-    $app->group('/groups/[{id}]', function (RouteCollectorProxy $group) {
+    $app->group('/groups[/{id}]', function (RouteCollectorProxy $group) {
         $group->any('', Actions\GroupsAction::class);
     })->add(ValidApiKeyMiddleware::class);
 
-    $app->group('/images/[{id}]', function (RouteCollectorProxy $group) {
+    $app->group('/images[/{id}]', function (RouteCollectorProxy $group) {
         $group->any('', Actions\ImagesAction::class);
     })->add(ValidApiKeyMiddleware::class);
 };
